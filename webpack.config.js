261e2e6a92105fe64f62a86a3bfa1c2569330b60
Tolga Ozkan <tolga_ozkan@epam.com>
2018-05-08 22:21:38 +0200

    HW2 - Webpack Conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,68 @@
 var webpack = require('webpack');
-const {resolve, join} = require('path');
+const { resolve, join } = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-var BUILD_DIR = resolve(__dirname, './dist');
 var APP_DIR = resolve(__dirname, './src');
 
-const config = {
-   entry: {
-     main: APP_DIR + '/index.js'
-   },
-   output: {
-     filename: 'bundle.js',
-     path: BUILD_DIR,
-   },
-   module: {
-    rules: [
-     {
-       test: /(\.css|.scss)$/,
-       use: [{
-           loader: "style-loader" // creates style nodes from JS strings
-       }, {
-           loader: "css-loader" // translates CSS into CommonJS
-       }, {
-           loader: "sass-loader" // compiles Sass to CSS
-       }]
-     },
-     {
-      test: /\.html$/,
-      loader: 'html-loader'
-     },
-     {
-       test: /\.(jsx|js)?$/,
-       use: [{
-         loader: "babel-loader",
-         options: {
-           cacheDirectory: true,
-           presets: ['react', 'es2015'] // Transpiles JSX and ES6
-         }
-       }]
-     }
+module.exports = function (env, options) {
+  const isProduction = options.mode === "production";
+
+  const config = {
+    context: join(__dirname, "src"),
+    entry: "./",
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? "none" : "source-map",
+
+    module: {
+      rules: [
+        {
+          test: /(\.css|.scss)$/,
+          use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+          }, {
+            loader: "css-loader" // translates CSS into CommonJS
+          }, {
+            loader: "sass-loader" // compiles Sass to CSS
+          }]
+        },
+        {
+          test: /\.tsx?$/,
+          loader: "awesome-typescript-loader",
+          exclude: /node_modules/,
+          options: {
+            useCache: true
+          }
+        },
+        {
+          test: /\.(jsx|js)?$/,
+          use: [{
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              presets: ['react', 'es2015'] // Transpiles JSX and ES6
+            }
+          }]
+        }
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "EPM-REACT",
+        hash: true,
+        template: resolve(__dirname, "./index.html")
+      })
     ],
 
-  },
-  resolve: {
-    modules: ['node_modules', join('src')]
-  },
-};
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      modules: ['node_modules', join('src')]
+    },
 
-module.exports = config;
+    devServer: {
+      contentBase: "./dist"
+    }
+  };
+
+  return config;
+};
